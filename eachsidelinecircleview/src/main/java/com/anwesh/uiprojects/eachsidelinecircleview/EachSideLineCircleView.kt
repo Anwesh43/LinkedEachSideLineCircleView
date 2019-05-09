82f2e859efc3eb153cb266c96792d6dc5fe56944
Anwesh43 <anweshthecool0@gmail.com>
2019-05-09 09:06:36 +0530

    adding ESLCNode class

--- eachsidelinecircleview/src/main/java/com/anwesh/uiprojects/eachsidelinecircleview/EachSideLineCircleView.kt
+++ eachsidelinecircleview/src/main/java/com/anwesh/uiprojects/eachsidelinecircleview/EachSideLineCircleView.kt
@@ -126,4 +126,48 @@ class EachSideLineCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ESLCNode(var i : Int, val state : State = State()) {
+
+        private var next : ESLCNode ?= null
+        private var prev : ESLCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ESLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawESLCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ESLCNode {
+            var curr : ESLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
