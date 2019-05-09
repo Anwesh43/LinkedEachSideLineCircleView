5bdd070ddb6a27fe7349f6f72852971332213898
Anwesh43 <anweshthecool0@gmail.com>
2019-05-09 08:42:28 +0530

    adding constants and libraries

--- eachsidelinecircleview/src/main/java/com/anwesh/uiprojects/eachsidelinecircleview/EachSideLineCircleView.kt
+++ eachsidelinecircleview/src/main/java/com/anwesh/uiprojects/eachsidelinecircleview/EachSideLineCircleView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.eachsidelinecircleview
 /**
  * Created by anweshmishra on 09/05/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.MotionEvent
+import android.view.View
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
