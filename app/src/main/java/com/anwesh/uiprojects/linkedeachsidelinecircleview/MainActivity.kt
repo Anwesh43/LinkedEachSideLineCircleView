221cea974e5c165f47f98578b506c27eb28b9201
Anwesh43 <anweshthecool0@gmail.com>
2019-05-09 09:17:41 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedeachsidelinecircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedeachsidelinecircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedeachsidelinecircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.eachsidelinecircleview.EachSideLineCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EachSideLineCircleView.create(this)
     }
 }
